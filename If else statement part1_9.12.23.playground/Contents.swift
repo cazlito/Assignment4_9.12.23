d7aa1207346c02df6b0f37d2dc614f1333003c46
cazlito <142947671+cazlito@users.noreply.github.com>
2023-09-12 13:30:09 -1000

    completed parts 1-4

--- If else statement part1_9.12.23.playground/Contents.swift
+++ If else statement part1_9.12.23.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//assignment 4
+//part 2
+
+var goodGrade = "I've been doing all my work"
+
+//create first if statement
+if goodGrade == "I've been doing all my homework" {
+    //if block
+    print("I should be getting an A")
+}
+
+//part 3
+else {
+    //else block
+    print("I NEED TO WORK HARDER RAAH")
+}
+
+//part 4
+var gradeA = true
+if gradeA == true {
+    print("I really enjoy this class")
+}
+else {
+    print("This class is not my parte")
+}
